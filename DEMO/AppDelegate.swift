b2864e9f451d521c17d89dd03b5365068b4326ee
Denis <denis.bogdanov@iacapps.com>
2021-09-01 07:09:30 +0300

    Another commit

--- DEMO/AppDelegate.swift
+++ DEMO/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("test")
         // Override point for customization after application launch.
         return true
     }
