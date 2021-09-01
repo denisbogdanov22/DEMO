1f951e3564e23d76bb6394ad46bb62700408ff46
Denis <denis.bogdanov@iacapps.com>
2021-09-01 07:04:34 +0300

    Add print label

--- DEMO/ViewController.swift
+++ DEMO/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("123")
+        print("123")
         // Do any additional setup after loading the view.
     }
 
